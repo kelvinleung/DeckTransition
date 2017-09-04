e596b28825b438791f874db260d5f337025ef8d8
Kelvin <et.kelvin@gmail.com>
2017-09-04 23:05:41 +0800

    init

---
+++ DeckTransition/ModalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ModalViewController.swift
+//  DeckTransition
+//
+//  Created by Kelvin Leung on 04/09/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+}
---
+++ DeckTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeckTransition
+//
+//  Created by Kelvin Leung on 04/09/2017.
+//  Copyright © 2017 Kelvin Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.viewWasTapped))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func viewWasTapped() {
+        let modal = ModalViewController()
+        present(modal, animated: true, completion: nil)
+    }
+
+}
